772f7b21b0f3fe65ad1425de05dbe72a4a8c0df3
tatailes19 <98753143+tatailes19@users.noreply.github.com>
2023-06-15 16:31:36 +0100

    Update color_detection.py

--- Python/py/color_detection.py
+++ Python/py/color_detection.py
@@ -1,14 +1,31 @@
 import cv2
 import numpy as np
 
-video=cv2.VideoCapture(0)
+video = cv2.VideoCapture(0)
 
-while True :
-    true,frame=video.read()
-    image=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    mask=cv2.inRange(image,np.array([0, 10, 60]),np.array([20, 150, 255]))
+upper, lower = np.array([0, 10, 60]),np.array([20, 150, 255])
 
-    cv2.imshow("mask",mask)
-    cv2.imshow("img",frame)
+"""
+colors = {'black': [[180, 255, 30], [0, 0, 0]],
+              'white': [[180, 18, 255], [0, 0, 231]],
+              'red1': [[180, 255, 255], [159, 50, 70]],
+              'red2': [[9, 255, 255], [0, 50, 70]],
+              'green': [[89, 255, 255], [36, 50, 70]],
+              'blue': [[128, 255, 255], [90, 50, 70]],
+              'yellow': [[35, 255, 255], [25, 50, 70]],
+              'purple': [[158, 255, 255], [129, 50, 70]],
+              'orange': [[24, 255, 255], [10, 50, 70]],
+              'gray': [[180, 18, 230], [0, 0, 40]]}
+              
+upper,lower = np.array(colors["yellow"][0]),np.array(colors["yellow"][1])
+"""
+
+while True:
+    true, frame = video.read()
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(image, upper, lower)
+
+    cv2.imshow("mask", mask)
+    cv2.imshow("img", frame)
 
     cv2.waitKey(1)
